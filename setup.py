cefbcec69dd93e6153843896690891e05b194fa1
Ethan Fast <ethanfast@Ethans-MacBook-Air-2.local>
2016-03-26 00:20:36 -0700

    deps

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 setup(
   name = 'metalang',
   packages = ['metalang'], # this must be the same as the name above
-  version = '0.4',
+  version = '0.42',
   description = 'A domain specific language that enables powerful code sharing',
   author = 'Ethan Fast',
   author_email = 'ejhfast@gmail.com',
   url = 'https://github.com/Ejhfast/meta', # use the URL to the github repo
-  download_url = 'https://github.com/Ejhfast/meta/tarball/0.3', # I'll explain this in a second
+  download_url = 'https://github.com/Ejhfast/meta/tarball/0.4',
   keywords = ['DSL', 'code sharing', 'metaprogramming'], # arbitrary keywords
   classifiers = [],
   install_requires=[
           'numpy',
           'dill',
+          'bson',
           'requests',
           'multiprocess'
   ]
